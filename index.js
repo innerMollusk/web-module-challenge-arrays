781e2b4e3a3c2c93c4541a0bf84f48cf4520821c
Ben Johnson <monk.sound.design@gmail.com>
2023-03-31 00:22:44 -0400

    edited task 2 for troubleshoot

--- index.js
+++ index.js
@@ -64,13 +64,13 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 
 
 function is31Flavors([]){
-  if(originalFlavors.length === 31) {
+  if(newArray.length === 31) {
 return true;
   } else {
     return false;
   }
  }
- is31Flavors(originalFlavors);
+ is31Flavors(newArray);
 
 
 
